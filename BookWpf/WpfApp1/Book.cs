c809f827eb8f7a83ff9e89da4575519f25467d70
binittamichael <92422074+binittamichael@users.noreply.github.com>
2021-10-17 14:35:19 +0530

    Add files via upload

---
+++ BookWpf/WpfApp1/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+   public  class Book
+    {
+        public string title;
+
+        public string author;
+
+        public int price;
+
+        public override string ToString()
+        {
+            return string.Format("{0} by {1} Price:₹ {2}", title, author, price);
+
+        }
+
+    }
+}
