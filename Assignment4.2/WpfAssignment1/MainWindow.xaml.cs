4fc9385fe8f4ca694d8013bd1806047ceb05c542
binittamichael <92422074+binittamichael@users.noreply.github.com>
2021-10-17 16:12:30 +0530

    Add files via upload

---
+++ Assignment4.2/WpfAssignment1/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using BookList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAssignment1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void HandleBookDetails(object sender, RoutedEventArgs e)
+        {
+            var book = booklist.SelectedItem as Book;
+            titleText.Text = book.Title;
+            authorText.Text = book.Author;
+            priceText.Text = "₹ " + book.Price;
+        }
+    }
+}
